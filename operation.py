7bf5a48733adb8742ce6b64b4a384a8c7a97aa7d
Kyle Schultz <kylews@uw.edu>
2021-10-17 21:10:46 -0700

    Encoder input working

--- operation.py
+++ operation.py
@@ -4,42 +4,35 @@ from encoder_position import calculate_encoder_position
 from encoder_functions import initialize_encoders
 from controller_functions import *
 from numpy import pi, sin, cos
+from os import path
 
-type = "encoder"
-f = 0.05
+
+#=== Change these to gather trials ===#
+type = "controller"
+#type = "hebi"
+#type = "encoder"
+f = 0.1
+T = 1/f
+
 
 #=== Global variables ===#
 
-# Initialize input filter:
-y1k_1 = 0 # y1[k-1]
-f1k_1 = 0 # f1[k-1]
-y2k_1 = 0 # y2[k-1]
-f2k_1 = 0 # f2[k-1]
+# Link lengths for 2 DOF:
+L1 = 0.29
+L2 = 0.22
 
 
 
-def input_filter(input, cutoff_freq, T):
-    # input freq in hz
-    global y1k_1
-    global f1k_1
-    global y2k_1
-    global f2k_1
-    
-    tau = 1/(2*np.pi*cutoff_freq)
-    y1 = (input[0] + f1k_1 - (1-2*tau/T)*y1k_1)/(2*tau/T+1)
-    y2 = (input[1] + f2k_1 - (1-2*tau/T)*y2k_1)/(2*tau/T+1)
-    y1k_1 = y1 
-    f1k_1 = input[0]
-    y2k_1 = y2
-    f2k_1 = input[1]
-    return np.array([y1,y2])
 
 def save_data(output):
     fs = str(f)
     f_r = fs.replace('.', '')
     save_name = "csv/{}_{}_1".format(type, f_r)
-    if path.exists(save_name) is True:
-        save_name = "csv/{}_{}_2".format(type, f_r)
+    for i in range(2, 100):
+        if path.exists(save_name) is True:
+            save_name = "csv/{}_{}_{}".format(type, f_r, i)
+        else:
+            break
 
     np.savetxt(save_name, np.array(output), delimiter=",")
     print("Data saved as:", save_name)
@@ -72,12 +65,12 @@ if __name__ == "__main__":
     target_ball = Ball(pos_i, target_ball_radius, "red", animation_canvas)
 
     if type == "controller":
-        gain = 500*vel_max
+        gain = 400*vel_max
         print(gain)
         input_ball = Ball(pos_i, input_ball_radius, "white", animation_canvas)
         pos_input = pos_i
 
-    if type == "hebi":
+    if type == "hebi" or type == "encoder":
         K = 0.1*vel_max*np.matrix([[1, 0],
                                    [0, 1]])
         freq = 100 # hz
@@ -85,13 +78,14 @@ if __name__ == "__main__":
         group.feedback_frequency = freq
         theta, omega, torque, hebi_limit_stop_flag = get_hebi_feedback(group, hebi_feedback)
         group_info = group.request_info()
+        if group_info is not None:
+            group_info.write_gains("csv/saved_gains.xml")
+
+    if type == "hebi":
         pos0 = calculate_hebi_position(group, hebi_feedback, offset = 0)
         offset = pos_i - pos0
         input_ball = Ball(calculate_hebi_position(group, hebi_feedback, offset), input_ball_radius, "white", animation_canvas)
         print(calculate_hebi_position(group, hebi_feedback, offset))
-        if group_info is not None:
-            group_info.write_gains("csv/saved_gains.xml")
-
 
     if type == "encoder":
         arduino = initialize_encoders()
@@ -103,19 +97,6 @@ if __name__ == "__main__":
 
     print("Get ready...")
     sleep(1)
-
-
-
-    #=== variables for 2 dof ===#
-    L1 = 0.29
-    L2 = 0.22
-    #======#
-
-    #=== variables for 3 dof ===#
-    # l1 = 0.268
-    # l2 = 0.472
-    #======#
-
 
     i = 0
     t0 = time()
@@ -147,10 +128,14 @@ if __name__ == "__main__":
        animation_window.update()
 
        if i == 0:
-           print("ready to operate...")
+           print("Ready to operate...")
            i = 1
 
+       if t > T:
+           save_data(output)
+           print("Trajectory complete, saving data")
+           break
 
        if keyboard.is_pressed('esc'):
-           # save_data(output)
+           print("Trajectory interupted")
            break
